726f12adca597659d3a23903a9c64775ec6140e0
rdsymlntsa <rdsymlntsa@gmail.com>
2026-02-09 17:55:15 +0530

    Added logout feature using cookie

--- chapter-18-cookiesandsessions/controllers/authController.js
+++ chapter-18-cookiesandsessions/controllers/authController.js
@@ -13,3 +13,8 @@ exports.postLogin=(req,res,next)=>{
  //req.isLoggedIn=true;
   res.redirect("/");
 }
+
+exports.postLogout=(req,res,next)=>{
+  res.cookie("isLoggedIn",false);
+  res.redirect("/login");
+}
--- chapter-18-cookiesandsessions/routes/authRouter.js
+++ chapter-18-cookiesandsessions/routes/authRouter.js
@@ -3,5 +3,6 @@ const authRouter=express.Router();
 const authController=require("../controllers/authController")
 authRouter.get("/login",authController.getLogin);
 authRouter.post("/login",authController.postLogin);
+authRouter.post("/logout",authController.postLogout);
 
 module.exports=authRouter;
