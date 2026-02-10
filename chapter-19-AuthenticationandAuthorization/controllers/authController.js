82cb0e72267aede0828b096e81fcebbe7a146e6b
rdsymlntsa <rdsymlntsa@gmail.com>
2026-02-10 13:51:55 +0530

    Added signup functionality

--- chapter-19-AuthenticationandAuthorization/controllers/authController.js
+++ chapter-19-AuthenticationandAuthorization/controllers/authController.js
@@ -6,6 +6,21 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    pageTitle: "Signup",
+    currentPage: "signup",
+    isLoggedIn: false,
+  });
+};
+exports.postSignup = (req, res, next) => {
+  console.log(req.body);
+  //req.session.isLoggedIn = true;
+  //res.cookie("isLoggedIn", true);
+  //req.isLoggedIn=true;
+  res.redirect("/login");
+};
+
 exports.postLogin = (req, res, next) => {
   console.log(req.body);
   req.session.isLoggedIn = true;
